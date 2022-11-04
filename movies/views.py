4f5f62c440249d2ac207916a5b8152de14d8692f
= <loveneverfails95@naver.com>
2022-11-04 10:48:47 +0900

    '영화 상세 페이지' 구현

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.views.decorators.http import require_safe
 from django.http.response import JsonResponse
 from .models import Movie
@@ -15,8 +15,11 @@ def index(request):
 
 @require_safe
 def detail(request, movie_pk):
-    pass
-
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    context = {
+        'movie': movie,
+    }
+    return render(request, 'movies/detail.html', context)
 
 @require_safe
 def recommended(request):
